595af8b708432db2a13fa6e10e6326a5cb28462e
Bilal Eroualen <ber.mailpro@gmail.com>
2023-03-08 14:00:13 +0100

    ajout de la photo sur la navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
