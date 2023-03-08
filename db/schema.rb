8bc4ac0daa4ca8c0624f869ed6a62a92dab792f4
sabri derbala <sabri.derbala@gmail.com>
2023-03-08 14:20:56 +0100

    page

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_143834) do
 
   create_table "event_users", force: :cascade do |t|
     t.string "user_address"
-    t.float "latitude"
-    t.float "longitude"
+    t.string "latitude"
+    t.string "longitude"
     t.bigint "mean_of_transport_id", null: false
     t.bigint "event_id", null: false
     t.bigint "user_id", null: false
