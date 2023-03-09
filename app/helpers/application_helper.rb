ec477d0d5f34ff9d9f7ca1892e022b58fdffcf86
Bilal Eroualen <ber.mailpro@gmail.com>
2023-03-09 11:29:04 +0100

    ajout de avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def avatar_for(user)
+    @avatar = user.avatar
+    if @avatar.empty?
+        @avatar_user = image_tag("https://pbs.twimg.com/media/C8QqGm3VYAEKUWC.jpg", alt: user.name)
+    else
+        @avatar_user = image_tag(@avatar.url, alt: user.name)
+    end
+    return @avatar_user
+  end
 end
