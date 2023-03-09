964e9b290ba1d6ada53fb1238579c2cc65aa8b51
Ana Candellier <ana.candellier@gmail.com>
2023-03-09 15:14:06 +0100

    2nd barycenter with speeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_133514) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_103630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_133514) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "speed"
+    t.float "duration"
+    t.float "distance"
     t.index ["event_id"], name: "index_event_users_on_event_id"
     t.index ["mean_of_transport_id"], name: "index_event_users_on_mean_of_transport_id"
     t.index ["user_id"], name: "index_event_users_on_user_id"
