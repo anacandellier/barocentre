f9a72421abff4b18d3f822af27f7d3671eb50dab
Ana Candellier <ana.candellier@gmail.com>
2023-03-13 16:57:10 +0100

    changing schema + fixing place id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_110454) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_153106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_110454) do
     t.bigint "event_id"
     t.string "name"
     t.float "rating"
-    t.integer "placeid"
+    t.string "placeid"
     t.string "photo"
     t.index ["event_id"], name: "index_bars_on_event_id"
   end
