830094d2fed5f1c987077becfff977ba0052d441
Noemie Nataf <noemienataf@gmail.com>
2023-03-13 18:11:31 +0100

    bouton share et front

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || super
   end
-  
+
   private
 
   def skip_pundit?
