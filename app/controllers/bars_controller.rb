1aab74a51cc225975acec231ae76aeb5e4a063aa
Ana Candellier <ana.candellier@gmail.com>
2023-03-13 15:46:47 +0100

    card bars sur la map

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -6,20 +6,12 @@ class BarsController < ApplicationController
     @event = Event.find(params[:event_id])
     @event.vote!
     @bars = Bar.where(event_id: params[:event_id])
-    @markers = @bars.map do |bar|
-      {
-        lat: bar.latitude,
-        lng: bar.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {bar: bar}),
-        marker_html: render_to_string(partial: "marker", locals: {bar: bar})
-      }
-     end
   end
 
   def map
     @event = Event.find(params[:event_id])
     @bars = Bar.where(event_id: params[:event_id])
-    @markers = @bars.map do |bar|votes
+    @markers = @bars.map do |bar|
       {
         lat: bar.latitude,
         lng: bar.longitude,
