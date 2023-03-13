53624d306fa1d2f6500999ce30c78b316f530054
Bilal Eroualen <ber.mailpro@gmail.com>
2023-03-13 10:55:28 +0100

    envoie

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -44,7 +44,7 @@ class EventUsersController < ApplicationController
         lng: eventuser.longitude,
         user_name: eventuser.user.email,
         info_window_html: render_to_string(partial: "info_window", locals: {event_user: eventuser} ),
-        marker_html: render_to_string(partial: "marker")
+        marker_html: render_to_string(partial: "marker", locals: {event_user: eventuser} )
       }
     end
   end
