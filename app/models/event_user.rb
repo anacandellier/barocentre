36be3eebdd9b1048e906bd1f3a6667043357435c
Ana Candellier <ana.candellier@gmail.com>
2023-03-14 12:57:33 +0100

    plein de trucs cools

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -3,9 +3,15 @@ class EventUser < ApplicationRecord
   belongs_to :user
   has_many :votes, dependent: :destroy
   validates :user_address, presence: true
-  validates_uniqueness_of :user_id, :scope => [:event_id]
-  validates :transport, presence: true, inclusion: { in: %w(driving transit bicycling)}
+  validates_uniqueness_of :user_id, scope: [:event_id]
+  validates :transport, presence: true, inclusion: { in: %w[driving transit bicycling] }
 
   geocoded_by :user_address
-  after_validation :geocode, if: :will_save_change_to_user_address?
+  after_validation :geocode, unless: :lon_lat_changed?
+
+  private
+
+  def lon_lat_changed?
+    longitude_changed? && latitude_changed?
+  end
 end
