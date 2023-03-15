ce460ca4f1c4ec304d0e47e135f5505fe5c97b90
Noemie Nataf <noemienataf@gmail.com>
2023-03-15 10:27:33 +0100

    eventuserlist

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,15 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share"
 export default class extends Controller {
-
   static values = {
     title: String,
     url: String,
-  }
-
-  connect() {
-    console.log(this.urlValue)
-    console.log(this.titleValue)
   }
 
   share() {
@@ -19,14 +13,9 @@ export default class extends Controller {
       text: `Hello, inscris toi à l'event : ${this.titleValue} en donnant ton adresse et ton moyen de transport pour qu'on trouve un bar qui arrange tout le monde`,
       url: this.urlValue
     }
-    console.log(sharingObj)
     if (navigator.share) {
-      // stocker dans des static 'values' (depuis l'HTML l'url de l'évenement)
-      // récupérer dans le JS et interpoller en tant que 'URL &
       navigator.share(sharingObj)
-        .then(() => {
-          console.log('Thanks for sharing!');
-        })
+        .then(() => { console.log('Thanks for sharing!') })
         .catch(console.error);
     } else {
       console.log(sharingObj)
