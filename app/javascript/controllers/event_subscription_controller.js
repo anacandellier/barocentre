348267fbcefb98d9c0303228124553d731315b9c
Ana Candellier <ana.candellier@gmail.com>
2023-03-16 11:38:23 +0100

    links > page vote

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -5,15 +5,18 @@ import { createConsumer } from "@rails/actioncable"
 export default class extends Controller {
   static values = {
     eventId: Number,
+    currentUserId: Number,
    }
   static targets = ["users", "trashcan"]
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "EventChannel", id: this.eventIdValue },
-      { received: (data) => {
-        this.usersTarget.insertAdjacentHTML("afterbegin", data)
-      }
-    })
+      { received: this.handleReceived.bind(this) }
+    )
   }
 
+  handleReceived(data) {
+    if (data.event_user) { this.usersTarget.insertAdjacentHTML("afterbegin", data.event_user) }
+    if (data.url && data.current_user_id != this.currentUserIdValue) { window.location.href = data.url }
+  }
 }
